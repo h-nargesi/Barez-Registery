3e9a75a9dbf30f0436a70ee09ac458eea5d6c7f9
Ryan Nargesi <ryan.nargesi.ai@gmail.com>
2023-09-21 07:52:06 +0330

    remove java-script method

--- go-steps.js
+++
@@ -1,8 +0,0 @@
-// Step 1
-$("#accept-rules").attr("checked", "checked");
-$('input[type="submit"]').click();
-
-//Step 2
-$('#idcard').val('0074990802');
-$('#phone').val('09125157305');
-$('button[type="submit"]').click();
